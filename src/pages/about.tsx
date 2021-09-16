3c557987440ef5258fc4510c3b7c6e625271bfa5
jaydee-cic <jd@cic.at>
2021-09-16 13:50:11 +0200

    adding some more information 'bout myself & highlighting links in text
    Signed-off-by: jaydee-cic <jd@cic.at>

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,6 +3,7 @@ import IndexPage from "./index";
 import NavComponent from "../components/NavComponent/NavComponent";
 // @ts-ignore
 import * as style from "./style/global.module.css"
+import {Link} from "gatsby";
 
 
 const AboutPage = () => {
@@ -12,7 +13,9 @@ const AboutPage = () => {
             <NavComponent> </NavComponent>
             <div className={style.container + " " + style.textCenter}>
                 <h1>About me</h1>
-                <p> I'm literally just retarded </p>
+                <p> I am 16 y/o apprentice web developer @ <a href={"https://www.cic.at/"}>CIC</a> Also, I'm heavily interested in motorsports & enthusiast cars :) </p>
+                <p>An overview of my private projects is available on <Link to={"/projects/"}>Projects</Link>, but please consider that most of my projects were started before I started working as a web developer. </p>
+
             </div>
         </main>
     )
