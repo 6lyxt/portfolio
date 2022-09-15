f499586ea9c4c6680e7d1272344e438ee16a740a
烏 <58228327+6lyxt@users.noreply.github.com>
2022-09-15 14:56:43 +0200

    updated age (i had birthday a while ago)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,7 +13,7 @@ const AboutPage = () => {
             <NavComponent> </NavComponent>
             <div className={style.container + " " + style.textCenter}>
                 <h1>About me</h1>
-                <p> I am 16 y/o apprentice web developer @ <a href={"https://www.cic.at/"}>CIC</a> Also, I'm heavily interested in motorsports & enthusiast cars :) </p>
+                <p> I am 17 y/o apprentice web developer @ <a href={"https://www.cic.at/"}>CIC</a> Also, I'm heavily interested in motorsports & enthusiast cars :) </p>
                 <p>An overview of my private projects is available on <Link to={"/projects/"}>Projects</Link>, but please consider that most of my projects were started before I started working as a web developer. </p>
 
             </div>
